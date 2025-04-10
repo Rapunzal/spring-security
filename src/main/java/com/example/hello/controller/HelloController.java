ba5c97bd4c337c63dd5d32097cb93a772c242500
Priyanka <priyankagjune@gmail.com>
2025-04-10 13:28:52 -0600

    implemented csrf token

--- src/main/java/com/example/hello/controller/HelloController.java
+++ src/main/java/com/example/hello/controller/HelloController.java
@@ -3,6 +3,7 @@ package com.example.hello.controller;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -43,5 +44,10 @@ public class HelloController {
 	public ResponseEntity<Person> savePerson(@RequestBody Person p){
 		Person p1 = service.createPerson(p);
 		return ResponseEntity.ok().body(p1);
+	}
+	
+	@GetMapping("/csrf-token")
+	public CsrfToken getCrsfToken(HttpServletRequest request) {
+		return (CsrfToken) request.getAttribute("_csrf");
 	}
 }
